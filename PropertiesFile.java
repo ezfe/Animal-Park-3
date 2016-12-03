c9aa136e90cf0bb776d1a621fd472671b0a0274f
Ezekiel Elin <ezekielelin@me.com>
2016-12-02 23:41:29 -0500

    Partial mountain stuff

--- PropertiesFile.java
+++ PropertiesFile.java
@@ -9,23 +9,23 @@ import java.io.File;
  * @version October 6, 2016
  */
 public class PropertiesFile {
-    
-	/*
-	 * Dimensions of board
-	 */
+
+    /*
+     * Dimensions of board
+     */
     int width = 0;
     int height = 0;
-    
+
     /**
      * Light energy
      */
     int light = 0;
-    
+
     /**
      * Species to add
      */
     ArrayList<SpeciesProperties> species = new ArrayList<>();
-    
+
     public PropertiesFile(String path) {
         Scanner sc = null;
         try {
@@ -34,26 +34,31 @@ public class PropertiesFile {
                 String line = sc.nextLine();
                 Scanner lsc = new Scanner(line);
                 if (lsc.hasNext()) {
-                	String keyword = lsc.next();
-                	if (keyword.equals("size")) {
-	                	try {
-	                		this.width = lsc.nextInt();
-	                		this.height = lsc.nextInt();
-	                	} catch (Exception e) {
-	                		System.out.println("ConfigError: Unable to load size");
-	                	}
-                	} else if (keyword.equals("light")) {
-                		try {
-                			this.light = lsc.nextInt();
-                		} catch (Exception e) {
-                			System.out.println("ConfigError: Unable to load light");
-                		}
-                	} else if (keyword.equals("species")) {
-                		SpeciesProperties sp = new SpeciesProperties(line);
-                		species.add(sp);
-                	}
+                    String keyword = lsc.next();
+                    if (keyword.equals("size")) {
+                        try {
+                            this.width = lsc.nextInt();
+                            this.height = lsc.nextInt();
+                        } catch (Exception e) {
+                            System.out.println("ConfigError: Unable to load size");
+                        }
+                    } else if (keyword.equals("light")) {
+                        try {
+                            this.light = lsc.nextInt();
+                        } catch (Exception e) {
+                            System.out.println("ConfigError: Unable to load light");
+                        }
+                    } else if (keyword.equals("species")) {
+                        SpeciesProperties sp = new SpeciesProperties(line);
+                        species.add(sp);
+                    } else if (keyword.equals("mountain")) {
+                        int x1 = lsc.nextInt();
+                        int y1 = lsc.nextInt();
+                        int x2 = lsc.nextInt();
+                        int y2 = lsc.nextInt();
+                    }
+                    lsc.close();
                 }
-                lsc.close();
             }
         } catch (Exception e) {
             System.out.println("ConfigError: Unable to load file");
